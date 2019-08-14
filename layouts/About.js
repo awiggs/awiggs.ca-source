1af8f85e2cfeb638a4cfcef0feb212c89ccac2ca
Andrew Wiggins <xandrewwiggins@gmail.com>
2019-08-13 22:13:50 -0700

    Added spacer, page titles

--- layouts/About.js
+++ layouts/About.js
@@ -1,27 +1,22 @@
 // Components
 import Boot from '../components/Boot'
 import Navbar from '../components/Navbar'
-// import Scroll from '../components/Scroll'
 import Footer from '../components/Footer'
-import Socials from '../components/Socials'
 import Content from '../content/about.mdx'
 
 export default ({ pageName }) => {
     return (
         <>
             <Boot title={pageName}/>
-            {/* <Scroll /> */}
             <div className='page work'>
                 <Navbar page='work' />
                 <div className='content-container container'>
                     <div className='row'>
-                        <div className='col content-col'>
+                        <div className='col'>
                             <Content />
-                            {/* <Socials /> */}
                         </div>
                     </div>
                 </div>
-                {/* <Socials /> */}
                 <Footer />
             </div>
         </>
